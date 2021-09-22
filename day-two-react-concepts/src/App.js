8cc1531fbb07ee9743db92d69a9c85b083dd85b1
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-09-23 00:51:03 +0600

    hooks, state, props, timer set

--- day-two-react-concepts/src/App.js
+++ day-two-react-concepts/src/App.js
@@ -1,16 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Device from './Component/Device/Device';
 
 function App() {
+  const [click, setClick] = useState(0);
+  const clickFunc = () => setClick(click + 1);
+  let [time, setTime] = useState(0);
+  setTimeout(function () {
+    setTime(time + 1);
+  }, 1000);
+  useEffect(() => {
+    console.log(time)
+  }, [time])
   return (
     <div className="App">
       <p>NPM = Node Package Manager</p>
       <p>CLI = Command Line Interface</p>
+      <p>Timer = {time}</p>
+      <p>Click = {click}</p>
       <button onClick={clickFunc}>Click me</button>
       <Device name='Xiaomi'></Device>
     </div>
   );
 }
-const clickFunc = () => console.log('clicked');
+
+
+
 
 export default App;
