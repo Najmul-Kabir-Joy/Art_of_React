42e09dffedf240b3c4b439e40c7c1a5d4762ac76
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-09-25 21:45:52 +0600

    used reducer instead of forof

--- simple-ecom-spa/src/components/Cart/Cart.js
+++ simple-ecom-spa/src/components/Cart/Cart.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const { cart } = props;
-    let total = 0;
-    for (const product of cart) {
-        total = total + product.price;
-    }
+    const total = cart.reduce((previous, product) => previous + product.price, 0)
+    // let total = 0;
+    // for (const product of cart) {
+    //     total = total + product.price;
+    // }
     return (
         <div>
             <h3>Order Summary</h3>
