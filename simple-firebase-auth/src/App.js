015c172cc5635acf708c3091e1e40139ab830115
Najmul-Kabir-Joy <najmulkabir178@gmail.com>
2021-10-08 22:29:03 +0600

    google and git auth system

---
+++ simple-firebase-auth/src/App.js
@@ -0,0 +1,78 @@
+import './App.css';
+import initAuth from './Firebase/firebase.initialize';
+import { getAuth, signOut, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
+import { useState } from 'react';
+
+initAuth();
+
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
+function App() {
+  const auth = getAuth();
+  const [user, setUser] = useState({});
+  const handleGitHubSignIn = () => {
+    signInWithPopup(auth, gitHubProvider)
+      .then(result => {
+        const user = result.user;
+        const { displayName, email, photoURL } = user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          img: photoURL
+        };
+        setUser(loggedInUser);
+      })
+  }
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({})
+      })
+  }
+  const handleGoolgeSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          img: photoURL
+        };
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  }
+  return (
+    <div className="App">
+      {
+        !user.name ? <div>
+          <button onClick={handleGoolgeSignIn}> Google Sign In</button>
+          <button onClick={handleGitHubSignIn}> GitHub Sign In</button>
+        </div>
+          :
+          <button onClick={handleSignOut}>Sign Out</button>
+      }
+      <br />
+      {
+        user.name &&
+        <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address: {user.email}</p>
+          <img src={user.img} alt="" />
+        </div>
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ simple-firebase-auth/src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBUX7yUnv0mAfPbDWmgslYnQvygmNYxYo4",
+    authDomain: "simple-firebase-auth-c2bd1.firebaseapp.com",
+    projectId: "simple-firebase-auth-c2bd1",
+    storageBucket: "simple-firebase-auth-c2bd1.appspot.com",
+    messagingSenderId: "857174137428",
+    appId: "1:857174137428:web:7bfea24ff4460e5a5eabc1"
+};
+
+export default firebaseConfig;
